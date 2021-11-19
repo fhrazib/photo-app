47fe2739b1018835c0342e9313c64117f96f501e
Razib <razib.fh@gmail.com>
2021-11-19 23:18:41 +0600

    Initial commit

---
+++ photo-app-user-service/src/main/java/com/fhr/photo/app/user/PhotoAppUserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.fhr.photo.app.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient // To enable this app as Eureka server's client; also need to add some properties
+public class PhotoAppUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppUserServiceApplication.class, args);
+	}
+
+}
---
+++ photo-app-user-service/src/test/java/com/fhr/photo/app/user/PhotoAppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhr.photo.app.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
